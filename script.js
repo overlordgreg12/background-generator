d7191c81ffef492b47fe3463977fbf36d5998463
overlordgreg12 <47050505+overlordgreg12@users.noreply.github.com>
2019-01-28 17:15:25 +1000

    adding starting project

---
+++ script.js
@@ -0,0 +1,33 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+var rbutton = document.querySelector("button");
+
+
+setGradient();
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+rbutton.addEventListener("click", function () {
+    console.log(getRandomColor()); 
+    color1.value = getRandomColor(); 
+    color2.value = getRandomColor(); 
+    setGradient(); 
+})
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+    css.textContent = body.style.background + ";"; 
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
